2fa5c426eada0bebbb40b864acacb9f3ea59ac68
swatizendewale <swatizendewale@gmail.com>
2020-06-25 17:35:20 +0530

    Application heath endpoint created

--- src/main/java/com/example/springBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springBootDemo/SpringBootDemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.springBootDemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 
@@ -14,5 +17,10 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
+	
+	 @Bean
+	    public HttpTraceRepository httpTraceRepository() {
+	        return new InMemoryHttpTraceRepository();
+	    }
 
 }
